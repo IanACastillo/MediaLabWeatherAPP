d258505fd07f1a53049433f507bd005f1a4e09df
Ian Castillo <ian.abraham.castillo@gmail.com>
2024-03-03 13:04:15 -0600

    Initial Commit

---
+++ MediaLabWeatherApp/MediaLabWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaLabWeatherAppApp.swift
+//  MediaLabWeatherApp
+//
+//  Created by Ian Castillo on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MediaLabWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
